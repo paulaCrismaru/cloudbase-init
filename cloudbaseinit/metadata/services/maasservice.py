e054facf52642bfb5f4b990e99dad3edab4d2247
Paula Madalina Crismaru <pcrismaru@cloudbasesolutions.com>
2017-04-18 14:57:32 +0300

    Implement networking MAAS
    Change-Id: I55841a10142ed2f1fce3c8e184f432efcd823335

--- cloudbaseinit/metadata/services/maasservice.py
+++ cloudbaseinit/metadata/services/maasservice.py
@@ -12,6 +12,9 @@
 #    License for the specific language governing permissions and limitations
 #    under the License.
 
+import json
+import netaddr
+import posixpath
 import re
 
 from oauthlib import oauth1
@@ -108,3 +111,96 @@ class MaaSHttpService(base.BaseHTTPMetadataService):
 
     def get_user_data(self):
         return self._get_cache_data('%s/user-data' % self._metadata_version)
+
+    def _get_nework_data(self, version='latest'):
+        path = posixpath.normpath(
+            posixpath.join(version, 'network_data.json'))
+        data = self._get_cache_data(path, decode=True)
+        if data:
+            return json.loads(data)
+
+    def get_network_details(self):
+        network_data = self._get_nework_data()
+        if network_data:
+            return self._parse_network_data(network_data)
+        else:
+            pass
+
+    def _parse_network_data(self, network_data):
+        if not network_data:
+            return None
+        LOG.debug(network_data)
+        network_l2_config = self._parse_l2_network_data(network_data)
+        LOG.debug(network_l2_config)
+        network_l3_config = self._parse_l3_network_data(network_data, network_l2_config)
+        LOG.debug(network_l3_config)
+        network_l4_config = self._parse_l4_network_data(network_data)
+        LOG.debug(network_l4_config)
+        return base.AdvancedNetworkDetails(network_l2_config, network_l3_config, network_l4_config)
+
+    def _parse_l2_network_data(self, network_data):
+        if not network_data:
+            return None
+        parsed_links = []
+        for link in network_data['config']:
+            if link.get('type') in ['nameserver']:
+                continue
+            parsed_link = {
+                'name': None,
+                'type': None,
+                'meta_type': None,
+                'mac_address': None,
+                'mtu': None
+            }
+            if link.get('id'):
+                parsed_link['name'] = link['id']
+            if link.get('type'):
+                parsed_link['meta_type'] = link['type']
+                parsed_link['type'] = 'phy'
+                if link['type'] in ['ovs', 'vif']:
+                    parsed_link['type'] = 'phy'
+                elif link['type'] in ['phy', 'bond', 'vlan']:
+                    parsed_link['type'] = link['type']
+            if link.get('mtu'):
+                parsed_link['mtu'] = link['mtu']
+            if link.get('mac_address'):
+                parsed_link['mac_address'] = link['mac_address'].upper()
+            parsed_links.append(parsed_link)
+        return parsed_links
+
+    def _parse_l3_network_data(self, network_data, network_l2_config):
+        if not (network_data and network_data['config']):
+            return None
+        parsed_networks = []
+        for network_config in network_data['config']:
+            if network_config.get('subnets'):
+                for subnet in network_config.get('subnets'):
+                    parsed_network = {
+                        'mac_address': None,
+                        'ip_address': None,
+                        'prefix': None,
+                        'gateway': None,
+                        'type': None
+                    }
+                    parsed_network['mac_address'] = network_config.get('mac_address')
+                    parsed_network['gateway'] = subnet.get('gateway')
+                    if subnet.get('address'):
+                        parsed_network['prefix'] = subnet.get('address').split('/')[1]
+                        parsed_network['ip_address'] = subnet.get('address').split('/')[0]
+                        version = netaddr.IPAddress(subnet.get('address')).version
+                        if version == 4:
+                            parsed_network['type'] = 'ipv4'
+                        elif version == 6:
+                            parsed_network['type'] = 'ipv6'
+
+                    parsed_networks.append(parsed_network)
+        return parsed_networks
+
+    def _parse_l4_network_data(self, network_data):
+        if not (network_data and network_data['config']):
+            return None
+        parsed_dnsses = []
+        for service in network_data['config']:
+            if service['type'] == 'nameserver':
+                parsed_dnsses.append(service['address'])
+        return {'dns_config': parsed_dnsses}
